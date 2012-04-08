243e1fb8c5a9c052803bea425b050604e51d6de2
Adam Noon <adam.ian.noon@gmail.com>
2012-04-08 15:47:01 -0600

    trying to pass data to javascript... bah

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,8 +2,14 @@ class GraphsController < ApplicationController
   before_filter :user_selected
 
   def index
+    @series = Array.new
+    @series << {
+      name: current_user.name,
+      data: current_user.steps.collect { |s| [s.date.to_time.to_i*1000, s.steps] }.to_json
+    }
+
     respond_to do |format|
-      format.html # index.html.slim
+      format.html { render 'index', locals: { series: @series } }
     end
   end
 end
